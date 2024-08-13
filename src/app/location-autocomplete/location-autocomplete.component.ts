6ef95d6e557c2d6b7eca6c8a6c087b04be8e7489
Julian Arce <117579382+anubiss10@users.noreply.github.com>
2024-08-13 13:03:58 +0200

    Front-end

---
+++ src/app/location-autocomplete/location-autocomplete.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+declare const google: any;
+
+@Component({
+  selector: 'app-location-autocomplete',
+  templateUrl: './location-autocomplete.component.html',
+  styleUrls: ['./location-autocomplete.component.css']
+})
+export class LocationAutocompleteComponent implements OnInit {
+  locationControl = new FormControl('');
+  filteredOptions: Observable<any[]> = new Observable<any[]>();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initializeAutocomplete();
+  }
+
+  private initializeAutocomplete(): void {
+    const input = document.getElementById('location') as HTMLInputElement;
+    if (input) {
+      const autocomplete = new google.maps.places.Autocomplete(input, { types: ['address'] });
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        if (place.geometry) {
+          console.log('Selected Place:', place.formatted_address);
+          // Aquí puedes hacer algo con la dirección seleccionada
+        }
+      });
+    }
+  }
+}
---
+++ src/app/weather/weather.model.ts
@@ -0,0 +1,6 @@
+export interface WeatherData {
+    location: string;
+    summary: string;
+    // Añade otros campos según la respuesta de tu API
+  }
+  
